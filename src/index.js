0dfe4ac127807777367c8d9256f06d6091d9c313
abhayHeliverse <siemonab@gmail.com>
2024-01-24 21:00:27 +0530

    added view

--- src/index.js
+++ src/index.js
@@ -16,6 +16,11 @@ const ordersRoute = require('./ordersRoute');
 //Creating table and seeding it with the data
 require('./createDatabase')
 
+//creating view for home route that facilitates all the apis
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.use('/api', ordersRoute)
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
